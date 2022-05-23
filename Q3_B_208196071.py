109ce528d0b781efd44ddd8d95770b355cd373bc
ShirGivon <Shirgivon4@gmail.com>
2022-05-23 14:02:46 +0300

    "tal answer - new"

--- Q3_B_208196071.py
+++ Q3_B_208196071.py
@@ -1,28 +1,25 @@
-from itertools import permutations
-
+import itertools
 
 # Function name: maxProfit(A)
 # Input: the A list (n - the length of the A list)
 # Output: the max difference from two elements -> What the question want's
 def maxProfit(A):
-    maxi = 0
-    a = list(permutations(A, 2))
-    print(a)
-    b = list(permutations(A, 4))
-    print(b)
-    for (x, y, z, w) in b:
-        if -x + y - z + w > maxi:
-            maxi = -x + y - z + w
-    for (x, y) in a:
-        if -x + y > maxi:
-            maxi = -x + y
-    return maxi
+    max_profit = 0
+    my_combinations = list(itertools.combinations(A, 4))
+    for (x, y, z, w) in my_combinations:
+        if (-x + y - z + w) > max_profit:
+            max_profit = (-x + y - z + w)
+    my_combinations = list(itertools.combinations(A, 2))
+    for (x, y) in my_combinations:
+        if (-x + y) > max_profit:
+            max_profit = (-x + y)
+    return max_profit
 
 
 if __name__ == '__main__':
     A = [5, 2, 10]
     print(maxProfit(A))
     B = [7, 2, 4, 8, 7]
-    print(maxProfit(B))
+    print(maxProfit([7, 2, 4, 8, 7]))
     C = [1, 2, 1, 2]
     print(maxProfit(C))
